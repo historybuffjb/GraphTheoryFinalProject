77af808c639e78f7b897d23d42ef80736b7d82ed
Johnathan Becker <historybuffjb@gmail.com>
2019-04-18 08:24:09 -0600

    Update

---
+++ src/main.py
@@ -0,0 +1,27 @@
+from dfpp import DFPP
+
+
+# test = {"a": ["d"], "b": ["c"], "c": ["b", "c", "d", "e"], "d": ["a", "c"], "e": ["c"], "f": []}
+# graph = Graph(test)
+# print("Vertices of graph:")
+# print(graph.vertices())
+# print("Edges of graph:")
+# print(graph.edges())
+# print("Add vertex:")
+# graph.add_vertex("z")
+# print("Vertices of graph:")
+# print(graph.vertices())
+# print("Add an edge:")
+# graph.add_edge({"a", "z"})
+# print("Vertices of graph:")
+# print(graph.vertices())
+# print("Edges of graph:")
+# print(graph.edges())
+# print('Adding an edge {"x","y"} with new vertices:')
+# graph.add_edge({"x", "y"})
+# print("Vertices of graph:")
+# print(graph.vertices())
+# print("Edges of graph:")
+# print(graph.edges())
+
+ob = DFPP("test.txt")
